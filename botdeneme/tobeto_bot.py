eca74634a1a8a347fcda97ddb6f94df4c78b2b34
Eray-Koc <koceray34@gmail.com>
2024-05-13 02:31:05 +0300

    Gereksiz kodlar kaldırıldı. Döngünün sonsuz olma nedeni ise nasıl olsa bir yerden sonra videolar bitecek ve video bulunamayınca program hata fırlatıp kendi kendine kapanacaktır

--- botdeneme/tobeto_bot.py
+++ botdeneme/tobeto_bot.py
@@ -6,14 +6,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from time import sleep
 
-def getmin(text):
-    i = 0
-    value = ""
-    while text[i] != '.':
-        value += text[i]
-        i += 1
-    return int(value)
-
 chromedriver_autoinstaller.install()
 driver = webdriver.Chrome()
 driver.get("https://tobeto.com/giris")
@@ -28,11 +20,11 @@ WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@i
 driver.find_element(By.XPATH, "//*[@id='lessons-tab-pane']/div/div/div[2]").click()
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[1]/div/div[1]")))
 driver.execute_script("window.scrollBy(0, 500)")
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[12]/div/div[2]/a")))  #SOFTSKILL IS BECERILERI XPATHI SADECE VIDEO ICEREN BASKA BIR PATH VERILEBILIR
-driver.find_element(By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[12]/div/div[2]/a").click() #SOFTSKILL IS BECERILERI XPATHI SADECE VIDEO ICEREN BASKA BIR PATH VERILEBILIR
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[12]/div/div[2]/a")))  #EGITIME GIT XPATH
+driver.find_element(By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[12]/div/div[2]/a").click() #EGITIME GIT XPATH
 WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, "//*[@id='my-player']/button")))
-i = 0
-while i < 100: #GIRILEN SOFTSKILLDE KAC TANE VIDEO VARSA BURADAKI SAYIYI DEGISTIRIN HAKKINDA KISMINDA BULABILIRSINIZ
+
+while True:
     sleep(2)
     try:
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='body']/div[4]/div/div[2]/div/div[2]/button")))
@@ -41,9 +33,6 @@ while i < 100: #GIRILEN SOFTSKILLDE KAC TANE VIDEO VARSA BURADAKI SAYIYI DEGISTI
         print("Yıldız gelmedi")
     driver.find_element(By.XPATH, "//*[@id='my-player']/button").click()
     sleep(0.4)
-    x = driver.find_element(By.XPATH, "//*[@id='my-player']/div[4]/div[7]/span[3]").text
-    duration_text = x.split(':')[0]
-    duration = int(duration_text)
     try:
         deg = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='vjs-playback-rate-value-label-my-player_component_254']")))
         if deg:
@@ -57,9 +46,7 @@ while i < 100: #GIRILEN SOFTSKILLDE KAC TANE VIDEO VARSA BURADAKI SAYIYI DEGISTI
         print("Else çalıştı")
     WebDriverWait(driver, 870).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='my-player']/button")))
     sleep(10)
-    i += 1
-# playbutton = driver.find_element(By.CLASS_NAME, "vjs-poster")
-# playbutton.click()
-sleep(7)
-#//*[@id="my-player"]/div[1]
+
+
+
 
