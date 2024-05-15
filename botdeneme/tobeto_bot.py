b080264d62bbfd3cd9aeaab99661a50a7ab22c84
Eray-Koc <koceray34@gmail.com>
2024-05-15 22:36:41 +0300

    Tobeto sayfasının güncellenmesi ile botumuz güncellenmiştir. Video izlerken karşımıza çıkan yıldızlı değerlendirme ekranında gelen güncelleme ile bir bozulma var mı test edilmedi. Sorun halinde geri dönüşlerinizi beklemekteyim.

--- botdeneme/tobeto_bot.py
+++ botdeneme/tobeto_bot.py
@@ -10,20 +10,17 @@ chromedriver_autoinstaller.install()
 driver = webdriver.Chrome()
 driver.get("https://tobeto.com/giris")
 driver.maximize_window()
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/div/form/input[1]")))
-driver.find_element(By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/div/form/input[1]").send_keys("") #MAILINIZI GIRIN
-driver.find_element(By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/div/form/input[2]").send_keys("") # SIFRENIZI GIRIN
-driver.find_element(By.XPATH, "//*[@id='__next']/div/main/section/div/div/div/div/form/button").click()
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='__next']/div/main/div[2]/div/div/div[4]/form/div[1]/input")))
+driver.find_element(By.XPATH, "//*[@id='__next']/div/main/div[2]/div/div/div[4]/form/div[1]/input").send_keys("") #MAILINIZI GIRIN
+driver.find_element(By.XPATH, "//*[@id='__next']/div/main/div[2]/div/div/div[4]/form/div[2]/input").send_keys("") # SIFRENIZI GIRIN
+driver.find_element(By.XPATH, "//*[@id='__next']/div/main/div[2]/div/div/div[4]/form/button[1]").click() #LOGIN
 WebDriverWait(driver, 8).until(EC.presence_of_element_located((By.XPATH, "//*[@id='lessons-tab']")))
-driver.find_element(By.XPATH, "//*[@id='lessons-tab']").click()
+driver.find_element(By.XPATH, "//*[@id='lessons-tab']").click() #EGITIMLERIM
 WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='lessons-tab-pane']/div/div/div[2]")))
-driver.find_element(By.XPATH, "//*[@id='lessons-tab-pane']/div/div/div[2]").click()
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[1]/div/div[1]")))
-driver.execute_script("window.scrollBy(0, 500)")
-WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[12]/div/div[2]/a")))  #EGITIME GIT XPATH
-driver.find_element(By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[12]/div/div[2]/a").click() #EGITIME GIT XPATH
+driver.find_element(By.XPATH, "//*[@id='lessons-tab-pane']/div/div/div[2]").click() #EGITIMLERIM GENISLETME BUTONU
+WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, "//*[@id='all-lessons-tab-pane']/div/div[14]/div/div[2]/button")))  #SOFTSKILL IS BECERILERI XPATHI SADECE VIDEO ICEREN BASKA BIR PATH VERILEBILIR
+driver.find_element(By.XPATH,  "//*[@id='all-lessons-tab-pane']/div/div[14]/div/div[2]/button").click() #SOFTSKILL IS BECERILERI XPATHI SADECE VIDEO ICEREN BASKA BIR PATH VERILEBILIR
 WebDriverWait(driver, 50).until(EC.presence_of_element_located((By.XPATH, "//*[@id='my-player']/button")))
-
 while True:
     sleep(2)
     try:
@@ -37,7 +34,6 @@ while True:
         deg = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='vjs-playback-rate-value-label-my-player_component_254']")))
         if deg:
             speedup = driver.find_element(By.XPATH, "//*[@id='vjs-playback-rate-value-label-my-player_component_254']").text
-            print(speedup)
             while speedup != '2x':
                 sleep(0.4)
                 driver.find_element(By.XPATH, "//*[@id='my-player']/div[4]/div[9]/button").click()
@@ -46,7 +42,3 @@ while True:
         print("Else çalıştı")
     WebDriverWait(driver, 870).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='my-player']/button")))
     sleep(10)
-
-
-
-
